7a48158d27ceea666af47e55a19c3ab6de2d1707
YooJiSang <digndign2@gmail.com>
2017-06-02 00:07:47 +0900

    소켓 분할 (데이터용 , 채팅용) 불필요 내용 삭제

--- cSocketManager.cpp
+++ cSocketManager.cpp
@@ -2,8 +2,6 @@
 #include "stdafx.h"
 
 cSocketManager::cSocketManager()
-	: dwSendTick(GetTickCount())
-	, dwReceiveTick(GetTickCount())
 {
 }
 
@@ -12,67 +10,20 @@ cSocketManager::~cSocketManager()
 }
 void cSocketManager::Setup()
 {
-	char message[30];
-	int strLen;
-	/* 윈도우 소켓 버전(2.2), 라이브러리 초기화 */
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-	{
-		//실패시 MessageBox를 띄운다.
-		int a = 3;
-	}
+}
 
-	/* 소켓 생성 , 소켓 핸들 반환 */
-	hSocket = socket(PF_INET, SOCK_STREAM, 0);			/// IPv4 주소, STREAM모드
-	if (hSocket == INVALID_SOCKET)						/// 소켓 비할당 예외처리
-	{
-		// 실패시 MessageBox를 띄워서 검사한다.
-		int b = 4;
-	}
-
-	memset(&servAddr, 0, sizeof(servAddr));
-	servAddr.sin_family = AF_INET;						/// IPv4 사용
-//	servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");		/// 호스트 IP주소
-	servAddr.sin_port = PORTNUM;						/// 접속할 포트 번호
-
-	/* 소켓 기반 연결요청 */
-	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)	/// Connect 예외처리
-	{
-		// 실패시 MessageBox를 띄워서 검사한다.
-		int c = 5;
-	}
-
-	/* Recv 함수호출을 통해 데이터 수신 */
-	strLen = recv(hSocket, message, sizeof(message) - 1, 0);						
-	if (strLen == -1)	/// Receive 예외처리
-	{
-		// MessageBox를 띄워서 검사한다.
-		int d = 6;
-	}
+void cSocketManager::Setup_Chat()
+{
 }
 
 void cSocketManager::SendData()
 {
-	if (GetTickCount() + SENDTICK > dwSendTick)
-	{
-		dwSendTick = GetTickCount();
-		SendData();
-	}
-
-	if (GetTickCount() + RECEIVETICK > dwReceiveTick)
-	{
-		dwReceiveTick = GetTickCount();
-	}
 }
 
 void cSocketManager::Update()
 {
-	/// Send , Receive 시간에 맞춰 수행
 }
 
 void cSocketManager::Close_Socket()
 {
-	// Client는 CloseSocket하면 된다. (HalfClose는 서버에서 수행)
-	closesocket(hSocket);
-	/* WinSoc 라이브러리 해제 */
-	WSACleanup();
 }
--- cSocketManager.h
+++ cSocketManager.h
@@ -1,27 +1,38 @@
 #pragma once
 #include "stdafx.h"
+// >> :
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+#include <process.h>
 #include <WinSock2.h>
 
-#define g_pSocketmanager cSocketManager::GetInstance() //
+#define g_pSocketmanager cSocketManager::GetInstance()
+
+#define BUF_SIZE 100
+#define NAME_SIZE
 #define HOSTIP "127.0.0.1"
-#define PORTNUM 1234
-#define RECEIVETICK 500
-#define SENDTICK 500
+#define DATA_PORT 1234
+#define CHAT_PORT 9090
+
 class cSocketManager
 {
 private:
 	SINGLETONE(cSocketManager);
 
-	DWORD	dwSendTick;
-	DWORD	dwReceiveTick;
 	WSADATA wsaData;
-	SOCKET hSocket;
-	SOCKADDR_IN servAddr;
-
+	// >> : For Chat
+	SOCKET hSockChat;
+	SOCKADDR_IN ChatServAdr;
+	HANDLE hSndThread, hRcvThread;
 public:
 	void Setup();
+	void Setup_Chat();
 	void SendData();			/// g_pDataManager에서 좌표값, 등등을 조합해서 Send한다.
 	void Update();
 	void Close_Socket();
+
+	unsigned WINAPI SendMsg(void * arg); // << : 채팅 Send 스레드 함수
+	unsigned WINAPI RecvMsg(void * arg); // << : 채팅 Recv 스레드 함수
 };
