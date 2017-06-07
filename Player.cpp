15444dbe0d1728cf91cc662505ad8051b837106c
jhchoi <cjh880118@gmail.com>
2017-06-07 13:13:24 +0900

    맵 오브젝트 추가 및 케릭터 getHeight추가

--- Player.cpp
+++ Player.cpp
@@ -13,10 +13,13 @@ Player::~Player()
 	if (player_Weapon) SAFE_DELETE(player_Weapon);
 }
 
-void Player::Setup(PLAYER_TYPE type)
+void Player::Setup(PLAYER_TYPE type, cMap* map)
 {
 	playerType = type;
 
+	
+	map->getSurface()->GetVecVertex();
+	
 	if (playerType == MALE)
 	{
 		player = new SkinnedMesh("Male/", "Male.X");
@@ -139,5 +142,31 @@ void Player::Render()
 		g_pD3DDevice->SetTransform(D3DTS_WORLD, &baseSphereLocal);
 		baseSphereMesh->DrawSubset(0);
 		g_pD3DDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
+	}
+}
+
+bool Player::GetHeight(IN float x, OUT float & y, IN float z, cMap* map)
+{
+	{
+		D3DXVECTOR3 vRayPos(x, 1000, z);
+		D3DXVECTOR3 vRayDir(0, -1, 0);
+		for (size_t i = 0; i < map->getSurface()->GetVecVertex.size(); i += 3) {
+			float u, v, f;
+
+			if (D3DXIntersectTri(
+				&map->getSurface()->GetVecVertex[i + 0],
+				&map->getSurface()->GetVecVertex[i + 1],
+				&map->getSurface()->GetVecVertex[i + 2],
+				&vRayPos,
+				&vRayDir,
+				&u,
+				&v,
+				&f
+			)) {
+				y = 1000 - f;
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- cMap.h
+++ cMap.h
@@ -16,8 +16,17 @@ private:
 	
 
 	cMapObject* m_pObjShackles;
-	cMapObject* m_pObjPrisonbar[12];
-	cMapObject* m_pObjPrisonbarRow[12];
+	cMapObject* m_pObjPrisonbar;
+	cMapObject* m_pObjPrisonbarRow;
+	cMapObject* m_pObjShelf;
+	cMapObject* m_pObjBox;
+	cMapObject* m_pObjWinebarrel;
+	cMapObject* m_pObjShelfHigh;
+	cMapObject* m_pObjPiano;
+	cMapObject* m_pObjBed;
+
+	cMapObject* m_pObjDesk;
+	cMapObject* m_pObjTable;
 	
 public:
 	cMap();
