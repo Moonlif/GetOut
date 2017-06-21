0eb59f7b593c9217a3d77ad6ccb33319173235fb
KKY <gy147k@naver.com>
2017-06-21 12:09:42 +0900

    12:09

--- ParticleManager.cpp
+++ ParticleManager.cpp
@@ -22,10 +22,14 @@ void ParticleManager::Render()
 
 void ParticleManager::CreateParticle()
 {
+	g_pSoundManager->AddSound("rain", "Sound/Background/rain.mp3", true, true);
+
 	BoundingBox rainBox;
 	rainBox.min = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
 	rainBox.max = D3DXVECTOR3(30.0f, 20.0f, 30.0f);
 
-	//rain = new Particle_Rain(rainBox, 800);
-	//rain->Initialize("Texture/particle/particle_rain.png");
+	rain = new Particle_Rain(rainBox, 800);
+	rain->Initialize("Texture/particle_rain.png");
+
+	g_pSoundManager->Play("rain", 0.4f);
 }
